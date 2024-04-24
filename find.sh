eeabd6a0bf52d1c648411d15b06e1ca41c8c756a
jjangg96 <jjangg96@gmail.com>
2024-04-24 13:16:10 +0900

    update bash script

--- find.sh
+++ find.sh
@@ -4,5 +4,5 @@ git clone https://github.com/0base-vc/fine-peers.git >/dev/null 2>&1
 apt install iputils-ping -y >/dev/null 2>&1
 cd fine-peers
 npm i >/dev/null 2>&1
-npm run start $1
+node src/index.js $1
 cd .. && rm -rf fine-peers
