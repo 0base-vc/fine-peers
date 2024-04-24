1b290c13688b0e601a6b34e12e2b584547eb07f0
jjangg96 <jjangg96@gmail.com>
2024-04-24 12:07:02 +0900

    update bash script and readme.md

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ```bash
 npm run start {chain}
 ```
+
+```bash
+curl -sSfL https://raw.githubusercontent.com/0base-vc/fine-peers/main/find.sh | bash -s - ${chain}
+```
--- find.sh
+++ find.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/0base-vc/fine-peers.git
-apt install iputils-ping -y
+git clone https://github.com/0base-vc/fine-peers.git >/dev/null 2>&1
+apt install iputils-ping -y >/dev/null 2>&1
 cd fine-peers
-npm i
+npm i >/dev/null 2>&1
 npm run start $1
 cd .. && rm -rf fine-peers
