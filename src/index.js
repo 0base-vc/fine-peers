94af13a3961379ce19aa8b9d69b80cdf6abf9ebb
jjangg96 <jjangg96@gmail.com>
2024-05-27 16:02:04 +0900

    peers can be an argument.

--- src/index.js
+++ src/index.js
@@ -22,28 +22,45 @@ const getPeers = async (chain) => {
 
 (async () => {
     if (!process.argv[2]) {
-        console.log("Please provide chain name as argument");
+        console.log("Please provide chain name or peers as argument");
         return;
     }
 
     const finePeers = []
-    const chain = process.argv[2];
-
-    setInterval(async () => {
-        const peers = await getPeers(chain);
+    if (process.argv[2].includes(',')) {
+        const peers = process.argv[2].split(',');
 
         for (const peer of peers) {
             const ip = getIP(peer);
             const result = await ping.promise.probe(ip);
             // if (result.alive) console.log(result.host, result.time);
-            if (result.time < 100) {
+            if (result.time < 10) {
                 finePeers.push(peer);
             }
         }
 
-        if (finePeers.length > 10) {
-            console.log(finePeers.join(','));
-            process.exit(0);
-        }
-    }, 5000);
+        console.log(finePeers.join(','));
+        process.exit(0);
+
+    } else {
+        const chain = process.argv[2];
+
+        setInterval(async () => {
+            const peers = await getPeers(chain);
+
+            for (const peer of peers) {
+                const ip = getIP(peer);
+                const result = await ping.promise.probe(ip);
+                // if (result.alive) console.log(result.host, result.time);
+                if (result.time < 100) {
+                    finePeers.push(peer);
+                }
+            }
+
+            if (finePeers.length > 10) {
+                console.log(finePeers.join(','));
+                process.exit(0);
+            }
+        }, 5000);
+    }
 })();
