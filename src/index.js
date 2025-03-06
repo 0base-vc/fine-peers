8f956f44cafe853ed5ef589cff59f13d28854e70
JinWoo Lee <jjangg96@gmail.com>
2025-03-06 16:32:18 +0900

    Fix IP parsing logic in `getIP` function.
    Correctly extract the IP address by adjusting the split operations on the peer string. This resolves potential issues with incorrectly formatted output from the `getIP` function.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const ping = require('ping');
 const axios = require('axios');
 
 const getIP = (peer) => {
+    //peer could be enode://0c5a4a3c0e81fce2974e4d317d88df783731183d534325e32e0fdf8f4b119d7889fa254d3a38890606ec300d744e2aa9c87099a4a032f5c94efe53f3fcdfecfe@34.64.176.79:30303 and 2784663b1c3fa2984b18cba3cffcd9af6c6ecef5@35.203.60.135:26656, parse IP
     if (peer) {
-        return peer.split(':')[0].split('@')[1];
+        return peer.split('@')[1].split(':')[0];
     }
+
+    // if (peer) {
+    //     return peer.split(':')[0].split('@')[1];
+    // }
 }
 
 const getPeers = async (chain) => {
